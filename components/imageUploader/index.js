29a2313dcea59c29040aa6c8a5c7d5cb452bb6d6
soberanoJr <tonysoberano@gmail.com>
2022-03-25 17:03:48 -0300

    image uploader

---
+++ components/imageUploader/index.js
@@ -0,0 +1,55 @@
+import { useRef } from "react";
+import { useEffect } from "react";
+
+export function ImageUploader({
+    className = '',
+    setImage,
+    imagePreview,
+    imagePreviewClassName = '',
+    settingRef,
+}) {
+    const inputReference = useRef(null);
+    useEffect(() => {
+        if(!settingRef) {
+            return;
+        }
+        settingRef(inputReference?.current);
+    }, [inputReference?.current]);
+    const openFileSelector = () => {
+        inputReference?.current?.click();
+    }
+    const changeImage = () => {
+        if(!inputReference?.current?.files?.length) {
+            return;
+        }
+        const file = inputReference?.current?.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onloadend = () => {
+            setImage({
+                preview: fileReader.result,
+                file
+            });
+        }
+    }
+    return (
+        <div className={`ImageUploaderContainer ${className}`} onClick={openFileSelector}>
+            {imagePreview && (
+                <div className="imagePreviewContainer">
+                    <img
+                        src={imagePreview}
+                        alt='Selected Image Preview'
+                        className={imagePreviewClassName}
+                    />
+                </div>
+            )}
+            <input 
+                type="file" 
+                className="hidden" 
+                accept="image/*"
+                ref={inputReference}
+                onChange={changeImage} 
+            />
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
+import { useRef } from "react";
+import { useState } from 'react';
 import Avatar from '../components/avatar';
 import Button from "../components/button"
+import { ImageUploader } from '../components/imageUploader';
 
 export default function Home() {
+  const [image, setImage] = useState(null);
+  const inputReference = useRef(null);
+  console.log(image);
   return (
     <>
       <h1>Hello, Zooagram</h1>
-      <Avatar />
-      <Button 
-        text={'Login'} 
-        //disabled={true}
-        //color='inverted_color' 
-        clickManager={() => console.log('click')} />
+      <button onClick={() => inputReference?.current?.click()}>Open File Selector</button>
+      <ImageUploader 
+        setImage={setImage} 
+        imagePreview={image?.preview}
+        settingRef={(ref) => inputReference.current = ref}
+      />
+      <div style={{width: 200}}>
+        <Avatar />
+        <Button 
+          text={'Login'} 
+          //disabled={true}
+          //color='inverted_color' 
+          clickManager={() => console.log('click')} />
+      </div>
     </>
   );
 }
